d37187eba95dcdeaa2ac4ef4d7df955b02d1946d
Tom Nachman <nachmant@post.bgu.ac.il>
2021-04-24 13:38:37 +0300

    test

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,77 @@
-function switch_content(id){
-    var option = ['Login_content','Register_content','Game_content','Welcome_content'];
-    option.forEach(element => {
-        if(id === element){
-            document.getElementById(element).style.display = 'block';
-        }
-        else{
-            document.getElementById(element).style.display = 'none';
-        }
-    });
-}
+$(document).ready(function(){
+    $("#Logout_user").hide();
+    $("#Welcome_content").show();
+    $("#Game_content").hide();
+    $("#Play_btn").hide();
+    $("#Play_navbar").hide();
+    $("#Login_content").hide();
+    //$("#Register_content").hide();
+});
+// Login Handle
+
+
+
+$("#Welcome_navbar").click(function() {
+    hide_everyhing();
+    $("#Logout_user").show();
+    $("#Game_content").show();
+});
+$("RegisterLogin_btn").click(function() {
+  hide_everyhing();
+  $("#Welcome_content").show();
+});
+
+$("#Play_btn, #Play_navbar").click(function(){
+    hide_everyhing();
+    $("#Game_content").show();
+});
+
+/*$("#Login_navbar").click(function() {
+    hide_everyhing();
+    $("#Login_content").show();
+});*/
+
+$("#Register_navbar").click(function() {
+    hide_everyhing();
+    $("#Register_content").show();
+});
+
+function hide_everyhing() {
+    $("#Welcome_content").hide();
+    $("#Game_content").hide();
+    $("#Login_content").hide();
+    $("#Register_content").hide();
+    //$("#About_content").hide();
+};
+// ---  Login + Register Scripts ---
+var $loginMsg = $('.loginMsg'),
+  $login = $('.login'),
+  $signupMsg = $('.signupMsg'),
+  $signup = $('.signup'),
+  $frontbox = $('.frontbox');
+
+$('#switch1').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.addClass("moving");
+  $signupMsg.toggleClass("visibility");
+
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+$('#switch2').on('click', function() {
+  $loginMsg.toggleClass("visibility");
+  $frontbox.removeClass("moving");
+  $signupMsg.toggleClass("visibility");
+
+  $signup.toggleClass('hide');
+  $login.toggleClass('hide');
+})
+
+setTimeout(function(){
+  $('#switch1').click()
+},1000)
+
+setTimeout(function(){
+  $('#switch2').click()
+},3000)
