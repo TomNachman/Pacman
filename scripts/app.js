be38ce09d897f374c0b54d0e70d40161f5e43b83
asafsalo <3asafi51@gmail.com>
2021-04-25 15:56:26 +0300

    asaf commmit canvas

--- scripts/app.js
+++ scripts/app.js
@@ -104,8 +104,8 @@ function Draw() {
 	for (var i = 0; i < 10; i++) {
 		for (var j = 0; j < 10; j++) {
 			var center = new Object();
-			center.x = i * 60 + 30;
-			center.y = j * 60 + 30;
+			center.x = i * 55 + 30;
+			center.y = j * 55 + 30;
 			if (board[i][j] == 2) {
 				context.beginPath();
 				context.arc(center.x, center.y, 30, 0.15 * Math.PI, 1.85 * Math.PI); // half circle
